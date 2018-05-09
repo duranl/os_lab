9d35f2ed3fbdcb395bdebdd9d3ce1d9907bfa44e
duranl <duranl@github.com>
2018-05-09 20:54:14 +0800

    Exercise 1 complete and add tester

--- kern/trap.c
+++ kern/trap.c
@@ -265,15 +265,19 @@ trap_dispatch(struct Trapframe *tf)
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
     if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
-        lapic_eoi();
-        sched_yield();
-        return;
-    }
-
+        
 	// Add time tick increment to clock interrupts.
 	// Be careful! In multiprocessors, clock interrupts are
 	// triggered on every CPU.
 	// LAB 6: Your code here.
+        lapic_eoi();
+        // To prevent calls multiple times
+        // due to multiprocessors
+        if (cpunum() == 0)
+            time_tick();
+        sched_yield();
+        return;
+    }
 
 
 	// Handle keyboard and serial interrupts.
